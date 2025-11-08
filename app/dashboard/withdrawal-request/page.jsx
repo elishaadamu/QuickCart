07cf9e47d0f6876b5402474f9101146fc7e5b3fa
elishaadamu <elishadamu97@gmail.com>
2025-11-08 17:24:00 +0100

    set new website

--- app/dashboard/withdrawal-request/page.jsx
+++ app/dashboard/withdrawal-request/page.jsx
@@ -241,9 +241,11 @@ const WithdrawalRequestPage = () => {
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm">
                       <span
-                        className={`px-2 py-1 text-xs rounded-full ${
+                        className={`px-2 py-2 text-[16px]  rounded-[10px]  ${
                           w.status === "completed"
                             ? "bg-green-100 text-green-800"
+                            : w.status === "approved"
+                            ? "bg-blue-100 text-blue-800"
                             : w.status === "failed"
                             ? "bg-red-100 text-red-800"
                             : "bg-yellow-100 text-yellow-800"
